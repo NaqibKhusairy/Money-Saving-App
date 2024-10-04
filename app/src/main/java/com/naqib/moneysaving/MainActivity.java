2a4e46761009e9d3d4f0c8703c038a012137c59c
NaqibKhusairy <naaqibkhuasiry@gmail.com>
2024-10-04 17:26:34 +0800

    define value for button

--- app/src/main/java/com/naqib/moneysaving/MainActivity.java
+++ app/src/main/java/com/naqib/moneysaving/MainActivity.java
@@ -3,12 +3,23 @@ package com.naqib.moneysaving;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText enterid , enterpassword;
+    TextView forgotpassword , signup;
+    Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterid=findViewById(R.id.enterid);
+        enterpassword=findViewById(R.id.enterpassword);
+        forgotpassword=findViewById(R.id.forgotpassword);
+        signup=findViewById(R.id.signup);
+        loginbtn=findViewById(R.id.loginbtn);
     }
 }
